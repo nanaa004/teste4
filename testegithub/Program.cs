d6078a8bb01cee41a27b03175c8e89ed21ca653a
nanaa004 <fernandaffranca004@gmail.com>
2025-04-15 08:27:40 -0300

    foi

---
+++ testegithub/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+bool teste1 = 5 > 2;
+
+bool teste2 = 5 >= 2;
+
+bool teste3 = 5 < 2;
+
+bool teste4 = 5 <= 2;
+
+bool teste5 = 5 == 2;
+
+bool teste6 = 5 != 2;
+
+Console.WriteLine(teste1);
+Console.WriteLine(teste2);
+Console.WriteLine(teste3);
+Console.WriteLine(teste4);
+Console.WriteLine(teste5);
+Console.WriteLine(teste6);
+*/
+
+int idade;
+
+idade = Convert.ToInt32(Console.ReadLine());
+
+bool maiorDeIdade = idade >= 18;
+
+Console.WriteLine(maiorDeIdade);
+
+
+
